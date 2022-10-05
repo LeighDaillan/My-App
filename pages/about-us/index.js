8b9b2c2f5d19aa517745a86184c326d9f83bf5e7
LeighDaillan <francodaillanleigh@gmail.com>
2022-10-05 19:52:42 +0800

    Added content on about us page

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,11 +1,44 @@
 import { Fragment } from "react";
-// import NavigationBar from "../Navigation/navigation-bar.js";
 
 const AboutUs = function () {
   return (
-    <Fragment>
-      {/* <NavigationBar page="About Us"></NavigationBar> */}
-      <h1 className="text-3xl font-bold underline">About Us Page</h1>
+    <Fragment className="font-pop">
+      <div className="grid grid-cols-2 ml-40 mt-20 gap-5 justify-content-center">
+        <div className="">
+          <p className="text-4xl font-bold ">
+            <span className="h-3 w-3 bg-gray-500 rounded-full inline-block mr-3 mb-2"></span>
+            I'm Daillan Leigh Franco, a <br />
+            future Front End Developer
+          </p>
+
+          <p className="mt-5 text-2xl indent-8">
+            Hi there! I was born and raised in the Philippines (and i know how
+            to speak kapampangan dialect) and am currently a college student. My
+            dream job is to be a Front End Developer, but being a Full Stack
+            Developer also sounds appealing. Please contact me if you like my
+            sample blog.
+          </p>
+        </div>
+
+        <div>
+          <h2 className="underline text-3xl font-bold mb-2">What I like?</h2>
+          <ul className="leading-8 text-2xl">
+            <li>Watching Movies</li>
+            <li>Playing Video Games</li>
+            <li>And Eating</li>
+          </ul>
+          <br />
+          <h2 className="underline text-3xl font-bold mb-2">
+            Where did I learn to program?
+          </h2>
+          <ul className="leading-8 text-2xl">
+            <li>Udemy</li>
+            <li>YouTube</li>
+            <li>Reading Documentation</li>
+            <li>College Program</li>
+          </ul>
+        </div>
+      </div>
     </Fragment>
   );
 };
