7fd09cf5f43decc252b710461e606bdd423e2897
LeighDaillan <francodaillanleigh@gmail.com>
2022-10-06 18:34:56 +0800

    Changes the table to grid and add custom css in globals.css

--- pages/FAQ/index.js
+++ pages/FAQ/index.js
@@ -7,96 +7,54 @@ const Faq = function () {
         Frequently Asked Questions (FAQ)
       </h1>
 
-      <div className="mt-16">
-        <table className="table-fixed mx-auto">
-          <tbody>
-            <tr>
-              <td className="text-5xl text-red-600 border-l-2 border-red-600 pl-3 inline-block ">
-                Q.
-              </td>
-              <td className=" text-xl ml-4 inline-block">
-                What is your favourite Programming Language?
-              </td>
-            </tr>
-            <tr>
-              <td className="text-5xl text-gray-500 border-l-2 border-gray-500 inline-block pl-3 ">
-                A.
-              </td>
-              <td className=" text-xl ml-4 inline-block text-gray-500">
-                JavaScript.
-              </td>
-            </tr>
-            <br />
-            <tr>
-              <td className="text-5xl text-red-600 border-l-2 border-red-600 pl-3 inline-block ">
-                Q.
-              </td>
-              <td className=" text-xl ml-4 inline-block">
-                Why do you like using JavaScript?
-              </td>
-            </tr>
-            <tr>
-              <td className="text-5xl text-gray-500 border-l-2 border-gray-500 inline-block pl-3 ">
-                A.
-              </td>
-              <td className=" text-xl ml-4 inline-block text-gray-500">
-                Because, its necessarry in Front End Development.
-              </td>
-            </tr>
-            <br />
-            <tr>
-              <td className="text-5xl text-red-600 border-l-2 border-red-600 pl-3 inline-block ">
-                Q.
-              </td>
-              <td className=" text-xl ml-4 inline-block">
-                Why did you study Information Technology?
-              </td>
-            </tr>
-            <tr>
-              <td className="text-5xl text-gray-500 border-l-2 border-gray-500 inline-block pl-3 ">
-                A.
-              </td>
-              <td className=" text-xl ml-4 inline-block text-gray-500">
-                Because I always wanted to learn how to code and build
-                something.
-              </td>
-            </tr>
-            <br />
-            <tr>
-              <td className="text-5xl text-red-600 border-l-2 border-red-600 pl-3 inline-block ">
-                Q.
-              </td>
-              <td className=" text-xl ml-4 inline-block">
-                What Operating System do you use in your machine?
-              </td>
-            </tr>
-            <tr>
-              <td className="text-5xl text-gray-500 border-l-2 border-gray-500 inline-block pl-3 ">
-                A.
-              </td>
-              <td className=" text-xl ml-4 inline-block text-gray-500">
-                Windows
-              </td>
-            </tr>
-            <br />
-            <tr>
-              <td className="text-5xl text-red-600 border-l-2 border-red-600 pl-3 inline-block ">
-                Q.
-              </td>
-              <td className=" text-xl ml-4 inline-block">
-                How many hours do you code in a day?
-              </td>
-            </tr>
-            <tr>
-              <td className="text-5xl text-gray-500 border-l-2 border-gray-500 inline-block pl-3 ">
-                A.
-              </td>
-              <td className=" text-xl ml-4 inline-block text-gray-500">
-                Minimum of 3 hours.
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="mt-16 grid grid-cols-2 items-center mr-40">
+        <span className="question-number justify-self-end">Q.</span>
+        <p className="question justify-self-start">
+          What is your favourite Programming Language?
+        </p>
+
+        <span className="answer-logo justify-self-end">A.</span>
+        <p className="answer justify-self-start">JavaScript.</p>
+
+        <span className="question-number justify-self-end">Q.</span>
+        <p className="question justify-self-start">
+          Why do you like using JavaScript?
+        </p>
+
+        <span className="answer-logo justify-self-end">A.</span>
+        <p className="answer justify-self-start">
+          Because, its necessarry in Front End Development.
+        </p>
+
+        <span className="question-number justify-self-end">Q.</span>
+        <p className="question justify-self-start">
+          Why did you study Information Technology?
+        </p>
+
+        <span className="answer-logo justify-self-end">A.</span>
+        <p className="answer justify-self-start">
+          Because I always wanted to learn how to code and build something.
+        </p>
+
+        <span className="question-number justify-self-end">Q.</span>
+        <p className="question justify-self-start">
+          Why did you study Information Technology?
+        </p>
+
+        <span className="answer-logo justify-self-end">A.</span>
+        <p className="answer justify-self-start">
+          Because I always wanted to learn how to code and build something.
+        </p>
+
+        <span className="question-number justify-self-end">Q.</span>
+        <p className="question justify-self-start">
+          What Operating System do you use in your machine?
+        </p>
+
+        <span className="answer-logo justify-self-end">A.</span>
+        <p className="answer justify-self-start">
+          Windows is the operating sys that I use.
+        </p>
       </div>
     </Fragment>
   );
