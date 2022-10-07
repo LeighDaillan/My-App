4ebbac4d8d4ebce3bd6d3054572aa5aaaa43ef22
LeighDaillan <francodaillanleigh@gmail.com>
2022-10-07 13:00:39 +0800

    update the source file for layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 
-import Layout from "../components/layout";
+import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
